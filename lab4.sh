afe605a30e217aa37669b12a82ae14c21a0c7b3f
KamilStasiewicz00 <dsw49626@student.dsw.edu.pl>
2024-03-17 15:14:55 +0100

    Flag change task done

--- lab4.sh
+++ lab4.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [ "$1" = "--date" ]; then
-    date
+    if [ "$2" = "-d" ]; then
+        date
+	fi
 fi
 
 if [ "$1" = "--logs" ]; then
 
-    if [ -z "$2" ]; then
+    if [ "$2" = "-l" ]; then
 
         for ((i=1; i<=100; i++))
         do
@@ -20,8 +22,7 @@ fi
 
 if [ "$1" = "--logs" ]; then
 
-    if [ -z "$2" ]; then
-        echo "If you want a specific number of file created, please add the number as an argument."
+    if [ "$2" = "-l" ]; then
         exit 1
     fi
 
@@ -37,12 +38,13 @@ if [ "$1" = "--logs" ]; then
 fi
 
 if [ "$1" = "--help" ]; then
-    echo "Usage: lab4.sh [option]"
-    echo "Options:"
-    echo "  --date		        Display current date"
-    echo "  --logs		        Create 100 numbered log files"
-    echo "  --logs <number_of_files>    Create specified number of log files"
-    echo "  --help                      Display this help message"
-
+    if [ "$2" = "-h" ]; then
+        echo "Usage: lab4.sh [option]"
+        echo "Options:"
+        echo "  --date		            Display current date"
+        echo "  --logs		            Create 100 numbered log files"
+        echo "  --logs <number_of_files>    Create specified number of log files"
+        echo "  --help	                    Display this help message"
+    fi
 fi
 
