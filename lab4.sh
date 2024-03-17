f1c0c569002320666e170d2d116f717cc9440d0e
KamilStasiewicz00 <dsw49626@student.dsw.edu.pl>
2024-03-17 14:52:12 +0100

    Logs 30 task done

--- lab4.sh
+++ lab4.sh
@@ -5,7 +5,29 @@ if [ "$1" = "--date" ]; then
 fi
 
 if [ "$1" = "--logs" ]; then
-    for ((i=1; i<=100; i++))
+
+    if [ -z "$2" ]; then
+
+        for ((i=1; i<=100; i++))
+        do
+            filename="log$i.txt"
+            echo "File name: $filename" > "$filename"
+            echo "Mother script name: lab4.sh" >> "$filename"
+            echo "Date of file creation: $(date)" >> "$filename"
+        done
+    fi
+fi
+
+if [ "$1" = "--logs" ]; then
+
+    if [ -z "$2" ]; then
+        echo "If you want a specific number of file created, please add the number as an argument."
+        exit 1
+    fi
+
+    num_files="$2"
+
+    for ((i=1; i<=$num_files; i++))
     do
         filename="log$i.txt"
         echo "File name: $filename" > "$filename"
