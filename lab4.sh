9d9f9fb693dc6bcfbab7d875efa577f29458845b
KamilStasiewicz00 <dsw49626@student.dsw.edu.pl>
2024-03-17 14:39:54 +0100

    Logs task done

--- lab4.sh
+++ lab4.sh
@@ -3,3 +3,14 @@
 if [ "$1" = "--date" ]; then
     date
 fi
+
+if [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++))
+    do
+        filename="log$i.txt"
+        echo "File name: $filename" > "$filename"
+        echo "Mother script name: lab4.sh" >> "$filename"
+        echo "Date of file creation: $(date)" >> "$filename"
+    done
+fi
+
